d53aaf4bc9523bb7cd01c4c665191d65fe6b9018
atsam10 <91466610+atsam10@users.noreply.github.com>
2022-04-24 09:18:14 +1000

    my code

---
+++ main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Barcelona </title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="stylecss/style.css">
+    <script src="js/bootstrap.bundle.js"></script>
+
+</head>
+<body>
+
+    <div class="container">
+
+    <?php
+    include("nav.php");
+    ?>
+
+    <div class="content">
+    <?php
+    include("home/home.php");
+    include("footer.php");
+    ?>
+    
+    </div>
+    </div>
+</body>
+</html>
